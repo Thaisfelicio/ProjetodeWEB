833cfa49d2dd437fc1c44bc497792d636492c392
Thaís Felício de Paula Silva <83433694+Thaisfelicio@users.noreply.github.com>
2023-06-27 11:14:28 -0300

    javascript do login e layout mobile de comentários

---
+++ trabalho_WEB/js/comentarios.js
@@ -0,0 +1,9 @@
+// const cardComentario = document.querySelector('.card');
+const comentar = document.querySelector('#comment_button');
+
+function criarCardComentario(nomeUsuario, comentario, qtdCurtidas){
+  
+}
+comentar.addEventListener("click", ()=>{
+  // document.body.appendChild(cardComentario);
+})
